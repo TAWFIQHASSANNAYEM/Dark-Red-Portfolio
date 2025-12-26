80c6d8d8bedfc47e5647130375de8cd212e28bfe
Tawfiq Hassan <tawfiqhassa018@gmail.com>
2025-12-27 04:42:47 +0600

    Fix STATIC_ROOT and static files confi for Render

--- Dark_Red_Portfolio/settings.py
+++ Dark_Red_Portfolio/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q%vv#n@odm#vj#3d-q2qraxrf^ix0$*^@05i@s-=$zs&wuwrtp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'https://tawfiq.onrender.com/']
 
@@ -37,7 +37,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 ROOT_URLCONF = 'Dark_Red_Portfolio.urls'
 
@@ -104,7 +109,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
